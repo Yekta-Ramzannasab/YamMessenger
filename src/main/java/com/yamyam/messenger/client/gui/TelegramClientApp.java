19708c98d7c8f07e66a9fa16387bce1d5c7302fc
Mobin Abedian <fermow.fermow85@gmail.com>
2025-07-21 08:58:35 +0330

    for test

--- src/main/java/com/yamyam/messenger/client/gui/TelegramClientApp.java
+++ src/main/java/com/yamyam/messenger/client/gui/TelegramClientApp.java
@@ -16,7 +16,7 @@ public class TelegramClientApp extends Application {
         URL fxmlLocation = getClass().getResource("/com/yamyam/messenger/client/gui/fxml/main-view.fxml");
         Parent root = FXMLLoader.load(fxmlLocation);
         Scene scene = new Scene(root, 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Helloo!");
         stage.setScene(scene);
         stage.show();
     }
