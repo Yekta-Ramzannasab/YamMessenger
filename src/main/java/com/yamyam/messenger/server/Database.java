c94f5891610f8adbf97a0f33360d243bb4f2acbf
Mobin Abedian <fermow.fermow85@gmail.com>
2025-07-23 16:56:04 +0330

    feat: add basic database connection class using JDBC

---
+++ src/main/java/com/yamyam/messenger/server/Database.java
@@ -0,0 +1,16 @@
+package com.yamyam.messenger.server;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database {
+    private static final String URL = "jdbc:postgresql://localhost:5432/YAM";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "mobin1234";
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+
+}
