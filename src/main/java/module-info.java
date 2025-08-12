762893aa3d8c18feddc82346371914764634ed18
Amiroooo0 <mesterdidi13831383@gmail.com>
2025-08-12 13:17:30 +0330

    fix: module-info.java synced with build.gradle

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,8 +7,15 @@ module com.yam.messenger {
     requires com.google.gson;
     requires java.sql;
 
+    // requirement of Email service
+    requires org.simplejavamail;
+    requires org.slf4j;
+    requires jakarta.mail;
+    requires org.simplejavamail.core;
+
     // allowing the fxml module to access your controllers deeply
     opens com.yamyam.messenger.client.gui to javafx.fxml;
+    opens com.yamyam.messenger.client.gui.controller to javafx.fxml;
 
     // making the main program package and shared classes available for execution
     exports com.yamyam.messenger.client.gui;
