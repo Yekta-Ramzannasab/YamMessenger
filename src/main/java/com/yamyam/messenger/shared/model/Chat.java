ae18f77a9a4237a47bb59c6bf80119fb6a269a65
Mobin Abedian <fermow.fermow85@gmail.com>
2025-08-15 12:22:16 +0330

    feat:Add Chat class and ChatType enum

---
+++ src/main/java/com/yamyam/messenger/shared/model/Chat.java
@@ -0,0 +1,38 @@
+package com.yamyam.messenger.shared.model;
+
+import java.sql.Timestamp;
+
+public class Chat {
+    private long chatId;
+    private Timestamp createdAt;
+    private ChatType type;
+    public Chat(long chatId, Timestamp createdAt, ChatType type){
+        this.chatId = chatId;
+        this.createdAt = createdAt;
+        this.type = type;
+    }
+
+    public long getChatId() {
+        return chatId;
+    }
+
+    public void setChatId(long chatId) {
+        this.chatId = chatId;
+    }
+
+    public ChatType getType() {
+        return type;
+    }
+
+    public void setType(ChatType type) {
+        this.type = type;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+}
