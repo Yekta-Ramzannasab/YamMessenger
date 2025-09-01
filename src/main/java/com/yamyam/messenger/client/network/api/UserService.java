19385ac027476f18810e9663fb123cf43a256e98
Mobin Abedian <fermow.fermow85@gmail.com>
2025-09-02 02:26:39 +0330

    feat(UserService): declare method to retrieve all users for UI integration

---
+++ src/main/java/com/yamyam/messenger/client/network/api/UserService.java
@@ -0,0 +1,8 @@
+package com.yamyam.messenger.client.network.api;
+
+import com.yamyam.messenger.shared.model.Users;
+import java.util.List;
+
+public interface UserService {
+    List<Users> getAllUsers();
+}
