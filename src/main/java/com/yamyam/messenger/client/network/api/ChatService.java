ab993c4bdc1a78b1b7914bc8772edde1139bade0
Mobin Abedian <fermow.fermow85@gmail.com>
2025-09-02 00:44:09 +0330

    feat(ChatService): add method to retrieve all private chats for a user

--- src/main/java/com/yamyam/messenger/client/network/api/ChatService.java
+++ src/main/java/com/yamyam/messenger/client/network/api/ChatService.java
@@ -1,11 +1,13 @@
 package com.yamyam.messenger.client.network.api;
 
 
+import com.yamyam.messenger.shared.model.PrivateChat;
+
 import java.util.List;
 
 public interface ChatService {
     void openChat(long meUserId, long targetUserId);
- //   List<Chat> getChatsForUser(long userId);
+    List<PrivateChat> getChatsForUser(long userId);
 
     //List<Message> getHistory(long meUserId, long targetUserId, int limit);
     //void send(long meUserId, long targetUserId, String text);
