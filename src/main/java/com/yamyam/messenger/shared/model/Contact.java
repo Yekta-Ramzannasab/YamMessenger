373537c2fc92d6d3a637c3287f254c1969d69926
Mobin Abedian <fermow.fermow85@gmail.com>
2025-09-02 02:12:56 +0330

    feat(model): add Contact class to represent user connections

---
+++ src/main/java/com/yamyam/messenger/shared/model/Contact.java
@@ -0,0 +1,25 @@
+package com.yamyam.messenger.shared.model;
+
+import java.time.LocalDateTime;
+
+public class Contact {
+    private final long ownerId;
+    private final long contactId;
+    private final LocalDateTime addedAt;
+
+    public Contact(long ownerId, long contactId, LocalDateTime addedAt) {
+        this.ownerId = ownerId;
+        this.contactId = contactId;
+        this.addedAt = addedAt;
+    }
+
+    public long getContactId() {
+        return contactId;
+    }
+    public LocalDateTime getAddedAt() {
+        return addedAt;
+    }
+    public long getOwnerId() {
+        return ownerId;
+    }
+}
