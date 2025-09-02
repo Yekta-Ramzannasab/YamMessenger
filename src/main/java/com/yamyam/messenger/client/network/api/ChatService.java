83fcee49328ca0dfa83d502a24b4c631a677cf44
Mobin Abedian <fermow.fermow85@gmail.com>
2025-09-02 15:32:41 +0330

    feat(ChatService): add getChannelByChatId method

--- src/main/java/com/yamyam/messenger/client/network/api/ChatService.java
+++ src/main/java/com/yamyam/messenger/client/network/api/ChatService.java
@@ -1,6 +1,7 @@
 package com.yamyam.messenger.client.network.api;
 
 
+import com.yamyam.messenger.shared.model.Channel;
 import com.yamyam.messenger.shared.model.Chat;
 import com.yamyam.messenger.shared.model.PrivateChat;
 
@@ -11,6 +12,7 @@ public interface ChatService {
     List<PrivateChat> getChatsForUser(String email);
     List<Chat> getGroupAndChannelChatsForUser(String email);
     PrivateChat getOrCreatePrivateChat(long meUserId, long otherUserId);
+    Channel getChannelByChatId(long chatId);
 
     //List<Message> getHistory(long meUserId, long targetUserId, int limit);
     //void send(long meUserId, long targetUserId, String text);
