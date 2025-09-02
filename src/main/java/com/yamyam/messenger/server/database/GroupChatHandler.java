3af6d239739a92735413fcebd9d80f021adae235
Mobin Abedian <fermow.fermow85@gmail.com>
2025-09-02 16:15:35 +0330

    feat(GroupChatHandler): implement group chat creation and loading logic via Database

---
+++ src/main/java/com/yamyam/messenger/server/database/GroupChatHandler.java
@@ -0,0 +1,24 @@
+package com.yamyam.messenger.server.database;
+
+import com.yamyam.messenger.shared.model.GroupChat;
+
+import java.sql.SQLException;
+
+public class GroupChatHandler {
+
+    private static final GroupChatHandler instance = new GroupChatHandler();
+
+    public static GroupChatHandler getInstance() {
+        return instance;
+    }
+
+    private GroupChatHandler() {}
+
+    public GroupChat createGroupChat(String name, String description, long creatorId, boolean isPrivate) throws SQLException {
+        return Database.insertGroupChat(name, description, creatorId, isPrivate);
+    }
+
+    public GroupChat loadGroupChat(long chatId) throws SQLException {
+        return Database.loadGroupChat(chatId);
+    }
+}
