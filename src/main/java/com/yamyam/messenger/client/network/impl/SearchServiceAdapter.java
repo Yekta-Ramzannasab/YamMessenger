d296e08a5a40e84160654addc6121d53d8590d18
Mobin Abedian <fermow.fermow85@gmail.com>
2025-09-02 11:56:26 +0330

    feat(SearchServiceAdapter): implement search logic using NetworkService and user email

---
+++ src/main/java/com/yamyam/messenger/client/network/impl/SearchServiceAdapter.java
@@ -0,0 +1,29 @@
+package com.yamyam.messenger.client.network.impl;
+
+import com.yamyam.messenger.client.network.NetworkService;
+import com.yamyam.messenger.client.network.api.SearchService;
+import com.yamyam.messenger.server.database.Database;
+import com.yamyam.messenger.server.database.SearchResult;
+import com.yamyam.messenger.shared.model.Users;
+
+import java.util.List;
+
+public class SearchServiceAdapter implements SearchService {
+    private final NetworkService net;
+
+    public SearchServiceAdapter(NetworkService net) {
+        this.net = net;
+    }
+
+    @Override
+    public List<SearchResult> search(String query, long meUserId) {
+        try {
+            Users me = Database.loadUser(meUserId);
+            assert me != null;
+            return net.fetchSearchResults(query, me.getEmail());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return List.of();
+        }
+    }
+}
