fb03b80f25b1b72eb21970806f1ffd2c037a4930
Mobin Abedian <fermow.fermow85@gmail.com>
2025-09-04 11:12:40 +0330

    feat(searchservice): define SearchService interface for unified access

--- src/main/java/com/yamyam/messenger/client/network/service/SearchService.java
+++ src/main/java/com/yamyam/messenger/client/network/service/SearchService.java
@@ -1,8 +1,10 @@
 package com.yamyam.messenger.client.network.service;
 
 import com.yamyam.messenger.server.database.SearchResult;
+import com.yamyam.messenger.shared.model.Users;
+
 import java.util.List;
 
 public interface SearchService {
-    List<SearchResult> search(String query, long meUserId);
+    List<Users> search(String query, long meUserId);
 }
