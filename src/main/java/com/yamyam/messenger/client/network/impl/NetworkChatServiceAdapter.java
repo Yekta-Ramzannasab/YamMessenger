5689ef727e491940542ca2faaa428e12d2efe15c
Amiroooo0 <mesterdidi13831383@gmail.com>
2025-09-05 18:02:51 +0330

    chore(network): remove commented sendMessage implementation from NetworkChatServiceAdapter
    cleans up outdated commented code to maintain adapter clarity and reduce noise

--- src/main/java/com/yamyam/messenger/client/network/impl/NetworkChatServiceAdapter.java
+++ src/main/java/com/yamyam/messenger/client/network/impl/NetworkChatServiceAdapter.java
@@ -81,15 +81,4 @@ public class NetworkChatServiceAdapter implements ChatService {
         System.out.println("Adapter: Delegating message sending to NetworkService...");
         this.networkService.sendChatMessage(chatId, text);
     }
-
-//    @Override
-//    public void sendMessage(long chatId, String text) {
-//        // The adapter simply hands the request to the corresponding method on the NetworkService
-//        try {
-//            NetworkService.sendChatMessage(chatId, text);
-//        } catch (IOException e) {
-//            throw new RuntimeException("Failed to send message", e);
-//        }
-//    }
-
 }
