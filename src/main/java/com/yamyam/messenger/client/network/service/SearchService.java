1a4b96e8cea28f52d2eb5650176e0ad390f4a046
Mobin Abedian <fermow.fermow85@gmail.com>
2025-09-06 03:44:52 +0330

    feat(SearchService): support combined user and chat search with ranking

--- src/main/java/com/yamyam/messenger/client/network/service/SearchService.java
+++ src/main/java/com/yamyam/messenger/client/network/service/SearchService.java
@@ -1,9 +1,9 @@
 package com.yamyam.messenger.client.network.service;
 
-import com.yamyam.messenger.shared.model.user.Users;
+import com.yamyam.messenger.server.database.SearchResult;
 
 import java.util.List;
 
 public interface SearchService {
-    List<Users> search(String query, long meUserId);
+    List<SearchResult> search(String query, long meUserId);
 }
